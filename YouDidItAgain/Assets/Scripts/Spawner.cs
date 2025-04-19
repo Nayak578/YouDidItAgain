2cfd347a4625de68b1f3593041f529e2f09a4a1e
Siddharth-732 <siddharthpn2003@gmail.com>
2025-04-19 15:26:47 +0530

    Added the game over

--- YouDidItAgain/Assets/Scripts/Spawner.cs
+++ YouDidItAgain/Assets/Scripts/Spawner.cs
@@ -14,7 +14,7 @@ public class Spawner : MonoBehaviour {
     public float maxForce = 22f;
     public float maxLifetime = 5f;
     public float spawnerDelay = 3f;
-
+    public bool condition = true;
     private List<GameObject> spawnQueue = new List<GameObject>();
     private System.Random rng = new System.Random();
 
@@ -30,6 +30,15 @@ public class Spawner : MonoBehaviour {
     private void OnDisable() {
         StopAllCoroutines();
     }
+    private Coroutine spawnCoroutine;
+
+    public void RestartSpawner()
+    {
+        if (spawnCoroutine != null)
+            StopCoroutine(spawnCoroutine);
+
+        spawnCoroutine = StartCoroutine(Spawn());
+    }
 
     private void RefillAndShuffleQueue() {
         spawnQueue.Clear();
@@ -43,25 +52,32 @@ public class Spawner : MonoBehaviour {
             spawnQueue[j] = temp;
         }
     }
-    private IEnumerator Spawn() {
+    private IEnumerator Spawn()
+    {
         yield return new WaitForSeconds(spawnerDelay);
 
-        while (enabled) {
-            if (spawnQueue.Count == 0) {
-                RefillAndShuffleQueue();
+        while (true)
+        {
+            if (!condition)
+            {
+                yield return null;
+                continue;
             }
+
+            if (spawnQueue.Count == 0)
+                RefillAndShuffleQueue();
 
             GameObject prefab = spawnQueue[0];
             spawnQueue.RemoveAt(0);
 
-            Vector3 position = new Vector3 {
+            Vector3 position = new Vector3
+            {
                 x = Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x),
                 y = Random.Range(spawnArea.bounds.min.y, spawnArea.bounds.max.y),
                 z = Random.Range(spawnArea.bounds.min.z, spawnArea.bounds.max.z)
             };
 
             Quaternion rotation = Quaternion.Euler(0f, 0f, Random.Range(minAngle, maxAngle));
-
             GameObject fruit = Instantiate(prefab, position, rotation);
             Destroy(fruit, maxLifetime);
 
@@ -71,4 +87,6 @@ public class Spawner : MonoBehaviour {
             yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
         }
     }
+
+
 }
